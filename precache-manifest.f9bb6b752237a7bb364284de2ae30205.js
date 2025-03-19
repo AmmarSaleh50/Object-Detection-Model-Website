a8766bb7cd2c235c9c9d3e5e1436bc5d24c75c7f
Ammar Saleh <ammar.alammar23@gmail.com>
2025-03-19 16:48:18 +0100

    Updates

---
+++ precache-manifest.f9bb6b752237a7bb364284de2ae30205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfb159ef761f26f2af36dbc658a60b6",
+    "url": "/Object-Detection-Model-Website/index.html"
+  },
+  {
+    "revision": "b3e8077b1f0bd5c0f2c8",
+    "url": "/Object-Detection-Model-Website/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2d7c58737d2620127bc4",
+    "url": "/Object-Detection-Model-Website/static/js/2.097b2a0c.chunk.js"
+  },
+  {
+    "revision": "bc620bd9cc8893328a5f453e25589d03",
+    "url": "/Object-Detection-Model-Website/static/js/2.097b2a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e8077b1f0bd5c0f2c8",
+    "url": "/Object-Detection-Model-Website/static/js/main.b6d90fa6.chunk.js"
+  },
+  {
+    "revision": "0a37ebfbb8bd2a8e911f",
+    "url": "/Object-Detection-Model-Website/static/js/runtime-main.c7fb2306.js"
+  }
+]);
